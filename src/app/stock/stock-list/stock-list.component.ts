34f3858769fe81c70edc9e988dbefd9d8148ed0a
Annelotte Fellinger <annelottef@gmail.com>
2022-07-05 16:31:09 +0200

    Deleting stock & plants

--- src/app/stock/stock-list/stock-list.component.ts
+++ src/app/stock/stock-list/stock-list.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {Stock} from "../../types/stock";
 import {StockService} from "../../services/stock.service";
+import {MatSnackBar} from "@angular/material/snack-bar";
 
 @Component({
   selector: 'app-stock-list',
@@ -11,7 +12,7 @@ export class StockListComponent implements OnInit {
   stockList: Stock[] | undefined;
   panelOpenState: boolean = false;
 
-  constructor(private stockService: StockService) {
+  constructor(private stockService: StockService, private _snackBar: MatSnackBar) {
   }
 
   ngOnInit(): void {
@@ -26,8 +27,24 @@ export class StockListComponent implements OnInit {
 
   deleteStockItem(id: number) {
     this.stockService.delete(id).subscribe(
-      () => this.getAll()
+      () => {
+        this.getAll();
+        this.openSnackBar("Stock item and associated plants in planner are deleted", "Ok");
+      }
     );
   }
 
+  archiveStock(item: Stock) {
+    item.available = false;
+    this.stockService.save(item).subscribe(
+      () => {
+        this.getAll();
+        this.openSnackBar("Stock item was archived", "Ok");
+      }
+    );
+  }
+
+  openSnackBar(message: string, action: string) {
+    this._snackBar.open(message, action, {duration: 3000});
+  }
 }
